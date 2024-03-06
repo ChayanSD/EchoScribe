dfab13a0526ea33f056ab93df3f20f7ab03c9326
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2024-03-06 14:07:26 +0600

    final setup and write the signup and login controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,73 @@
-import { Hono } from 'hono'
+import {Hono} from 'hono'
+import {PrismaClient} from '@prisma/client/edge'
+import {withAccelerate} from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new Hono<{
+    Bindings: {
+        DATABASE_URL: string
+    }
+}>();
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password
+      }
+    });
+
+    const token = await sign({ id : user.id},c.env.JWT_SECRET)
+    return c.json({jwt : token});
+
+  } catch(e) {
+    return c.status(403);
+  }
 })
 
+app.post('/api/v1/signin', async (c) => {
+   const prisma = new PrismaClient({
+       datasourceUrl : c.env.DATABASE_URL
+   }).$extends(withAccelerate());
+
+   const body =await c.req.json();
+   try {
+       const user = await prisma.user.findUnique({
+           where : {
+               email : body.email
+           }
+       });
+       if (!user) {
+           c.status(403);
+           return c.json({ error: "user not found" });
+       }
+
+       const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+       return c.json({ jwt });
+   }catch (e){
+       return c.status(403);
+   }
+
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+    const id = c.req.param('id')
+    console.log(id)
+    return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+    return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+    return c.text('signin route')
+})
 export default app
